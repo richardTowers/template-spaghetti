3d1c2542e6e04a8e646f17749e06288ba48c2012
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-22 18:29:22 +0100

    Emit if statements
    code looks a bit more future proof now.
    
    The currying is unecessary - should probably get rid of that.

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
 const parser = require('nunjucks/src/parser')
 
-const walk = cb => node => (
-  cb(node),
-  node.children &&
-    node.children.forEach(child => walk(cb)(child))
-)
+const walk = cb => node => {
+  const children = node.children || []
+  return [cb(node)].concat(...children.map(c => walk(cb)(c))).join('')
+}
 
-const templateDataEmitter = result => node => `${result}${node.value}`
-const symbolEmitter = result => node => `${result}<%= ${node.value} %>`
-const lookupValEmitter = result => node => `${result}<%= ${node.target.value}.${node.val.value} %>`
-const funCallEmitter = result => node => `${result}<%= ${node.name.target.value}.${node.name.val.value}() %>`
-const ifEmitter = result => node => `<% if ${node.cond.value} %>TODO - implement the body<% end %>`
+const emitter = node => walk((node) => {
+  switch (node.typename) {
+    case 'Root':
+    case 'NodeList':
+    case 'Output':
+      // These are just groupings, so we can skip them
+      break
+    case 'TemplateData':
+      return templateDataEmitter(node)
+    case 'Symbol':
+      return symbolEmitter(node)
+    case 'LookupVal':
+      return lookupValEmitter(node)
+    case 'FunCall':
+      if (node.args.children.length) {
+        throw new Error('Function calls with arguments are not supported yet')
+      }
+      return funCallEmitter(node)
+    case 'If':
+      return ifEmitter(node)
+    default:
+      console.error(node)
+      throw new Error(`Unhandled node type ${node.typename}`)
+  }
+})(node)
+const templateDataEmitter = node => `${node.value}`
+const symbolEmitter = node => `<%= ${node.value} %>`
+const lookupValEmitter = node => `<%= ${node.target.value}.${node.val.value} %>`
+const funCallEmitter = node => `<%= ${node.name.target.value}.${node.name.val.value}() %>`
+const ifEmitter = node => `<% if ${node.cond.value} %>${emitter(node.body)}<% end %>`
 
 module.exports = {
   nunjucksToErb: njkInput => {
-    let result = ''
     const ast = parser.parse(njkInput)
-    walk((node) => {
-      switch (node.typename) {
-        case 'Root':
-        case 'Output':
-          // These are just groupings, so we can skip them
-          break
-        case 'TemplateData':
-          result = templateDataEmitter(result)(node)
-          break
-        case 'Symbol':
-          result = symbolEmitter(result)(node)
-          break
-        case 'LookupVal':
-          result = lookupValEmitter(result)(node)
-          break
-        case 'FunCall':
-          if (node.args.children.length) {
-            throw new Error('Function calls with arguments are not supported yet')
-          }
-          result = funCallEmitter(result)(node)
-          break
-        case 'If':
-          // TODO this doesn't work yet because we need to recurse to print the
-          // body and I haven't got the code right yet
-          result = ifEmitter(result)(node)
-          break
-        default:
-          console.error(node)
-          throw new Error(`Unhandled node type ${node.typename}`)
-      }
-    })(ast)
-    return result
+    return emitter(ast)
   }
 }
