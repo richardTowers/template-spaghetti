466c101cf619c377523bd8c022dedcd3d54dbe05
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-10-22 18:00:56 +0100

    Beginnings of an ERB emitter

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// TODO implement everything
+const parser = require('nunjucks/src/parser')
+
+const walk = cb => node => (
+  cb(node),
+  node.children &&
+    node.children.forEach(child => walk(cb)(child))
+)
+
+const templateDataEmitter = result => node => `${result}${node.value}`
+const symbolEmitter = result => node => `${result}<%= ${node.value} %>`
+const lookupValEmitter = result => node => `${result}<%= ${node.target.value}.${node.val.value} %>`
+const funCallEmitter = result => node => `${result}<%= ${node.name.target.value}.${node.name.val.value}() %>`
+const ifEmitter = result => node => `<% if ${node.cond.value} %>TODO - implement the body<% end %>`
+
+module.exports = {
+  nunjucksToErb: njkInput => {
+    let result = ''
+    const ast = parser.parse(njkInput)
+    walk((node) => {
+      switch (node.typename) {
+        case 'Root':
+        case 'Output':
+          // These are just groupings, so we can skip them
+          break
+        case 'TemplateData':
+          result = templateDataEmitter(result)(node)
+          break
+        case 'Symbol':
+          result = symbolEmitter(result)(node)
+          break
+        case 'LookupVal':
+          result = lookupValEmitter(result)(node)
+          break
+        case 'FunCall':
+          if (node.args.children.length) {
+            throw new Error('Function calls with arguments are not supported yet')
+          }
+          result = funCallEmitter(result)(node)
+          break
+        case 'If':
+          // TODO this doesn't work yet because we need to recurse to print the
+          // body and I haven't got the code right yet
+          result = ifEmitter(result)(node)
+          break
+        default:
+          console.error(node)
+          throw new Error(`Unhandled node type ${node.typename}`)
+      }
+    })(ast)
+    return result
+  }
+}
--- test.js
+++ test.js
@@ -1,5 +1,28 @@
-describe('govuk-frontend-templates', () => {
-  it('should not break maths', () => {
-    expect(1 + 1).toBe(2)
+const index = require('./index')
+
+describe('nunjucksToErb', () => {
+  it('should emit template data', () => {
+    const result = index.nunjucksToErb('some thing')
+    expect(result).toEqual('some thing')
+  })
+
+  it('should emit symbols', () => {
+    const result = index.nunjucksToErb('key = {{value}}')
+    expect(result).toEqual('key = <%= value %>')
+  })
+
+  it('should emit value lookups', () => {
+    const result = index.nunjucksToErb('key = {{value.things}}')
+    expect(result).toEqual('key = <%= value.things %>')
+  })
+
+  it('should emit function calls', () => {
+    const result = index.nunjucksToErb('key = {{value.method()}}')
+    expect(result).toEqual('key = <%= value.method() %>')
+  })
+
+  it('should emit if statements', () => {
+    const result = index.nunjucksToErb('key = {% if true %}Thing{% endif %}')
+    expect(result).toEqual('key = <% if true %>Thing<% end %>')
   })
 })
